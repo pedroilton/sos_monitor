b51b52389a26bb960a1f9b33a84c9ec11e7df10b
Pedro Ilton <pedroilton@gmail.com>
2020-12-10 14:47:10 -0300

    Referências à tabela user corrigidas

--- app/models/classes_student.rb
+++ app/models/classes_student.rb
@@ -1,4 +1,4 @@
 class ClassesStudent < ApplicationRecord
   belongs_to :university_class
-  belongs_to :user
+  belongs_to :student, class_name: 'User', foreign_key: 'student_id', validate: true
 end
--- app/models/university_class.rb
+++ app/models/university_class.rb
@@ -1,6 +1,6 @@
 class UniversityClass < ApplicationRecord
   belongs_to :discipline
-  belongs_to :user
+  belongs_to :professor, class_name: 'User', foreign_key: 'professor_id', validate: true
   belongs_to :academic_year
 
   has_many :classes_students
