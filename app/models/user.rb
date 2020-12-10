72b987eac1da9fa07faffce6fcd5ea5b626dce10
Pedro Ilton <pedroilton@gmail.com>
2020-12-10 16:32:36 -0300

    Login pela matriculaaaaaaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  belongs_to :course
+         :recoverable, :rememberable, :validatable, authentication_keys: [:registration]
 
   has_many :classes_students, foreign_key: 'student_id'
   has_many :university_classes, foreign_key: 'professor_id'
@@ -13,6 +11,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :nickname, presence: true
+  validates :course_id, presence: false
 
   def student?
     student
@@ -28,5 +27,17 @@ class User < ApplicationRecord
 
   def admin?
     admin
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    registration = conditions.delete(:registration)
+    if registration
+      where(conditions).where(["lower(registration) = :value", { value: registration.downcase }]).first
+    elsif conditions[:registration].nil?
+      where(conditions).first
+    else
+      where(registration: conditions[:registration]).first
+    end
   end
 end
