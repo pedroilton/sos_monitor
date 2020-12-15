689469438e6c6974e8880a98bb8f3d629a419f02
Uellington Cortes <code.uellington@gmail.com>
2020-12-15 17:39:22 -0300

    fix bugs to add university class

--- app/controllers/university_classes_controller.rb
+++ app/controllers/university_classes_controller.rb
@@ -1,4 +1,6 @@
 class UniversityClassesController < ApplicationController
+  before_action :set_university_class, only: %i[edit update show]
+
   def index
     @university_classes = policy_scope(UniversityClass).sort_by do |university_class|
       [university_class.discipline.title, university_class.title]
@@ -6,7 +8,6 @@ class UniversityClassesController < ApplicationController
   end
 
   def show
-    @university_class = UniversityClass.find(params[:id])
     @class_monitor = ClassMonitor.new
     @available_monitors = User.all.select(&:student?).reject do |user|
       @university_class.students.include?(user) || @university_class.class_monitors.map(&:student).include?(user)
@@ -16,13 +17,22 @@ class UniversityClassesController < ApplicationController
 
   def new
     @disciplines = Discipline.all
+    @professors = User.where(professor: true)
     @university_class = UniversityClass.new
+
     authorize @university_class
   end
 
   def create
     @university_class = UniversityClass.new(university_class_params)
     authorize @university_class
+    @discipline = Discipline.find(params[:university_class][:discipline_id])
+    @professor = User.find(params[:university_class][:professor_id])
+    @academicYear == AcademicYear.where(['start_date <= ? and end_date >= ?', Date.today, Date.today]).first
+
+    @university_class.discipline = @discipline
+    @university_class.professor = @professor
+    @university_class.academic_year = @academicYear
     if @university_class.save
       redirect_to new_university_class_path
     else
@@ -31,6 +41,7 @@ class UniversityClassesController < ApplicationController
   end
 
   def edit
+    @professors = User.where(professor: true)
     authorize @university_class
   end
 
