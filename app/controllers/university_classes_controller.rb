40d3449d146f5b0dd3b7a8c5e7dba82e4fb5c345
Uellington Cortes <code.uellington@gmail.com>
2020-12-15 11:03:22 -0300

    add classes index new and edit

--- app/controllers/university_classes_controller.rb
+++ app/controllers/university_classes_controller.rb
@@ -1,8 +1,45 @@
 class UniversityClassesController < ApplicationController
   def index
     @universityClasses = policy_scope(UniversityClass).all
-    # .includes(:discipline)
-    # @disciplines = Discipline.all.sort_by { |discipline| discipline.title }
-    # @universityClasses = policy_scope(UniversityClass).all.order(discipline: :desc)
+  end
+
+  def new
+    @disciplines = Discipline.all
+    @universityClass = UniversityClass.new
+    authorize @universityClass
+  end
+
+  def create
+    @universityClass = UniversityClass.new(universityClass_params)
+    authorize @universityClass
+    if @universityClass.save
+      redirect_to new_universityClasss_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @universityClass
+  end
+
+  def update
+    authorize @universityClass
+
+    if @universityClass.update(universityClass_params)
+      redirect_to universityClasss_path, notice: 'Turma atualizada com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_universityClass
+    @universityClass = UniversityClass.find(params[:id])
+  end
+
+  def universityClass_params
+    params.require(:universityClass).permit(:title, :code, :active)
   end
 end
--- app/policies/university_class_policy.rb
+++ app/policies/university_class_policy.rb
@@ -4,4 +4,28 @@ class UniversityClassPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index
+    admin_or_profesor?
+  end
+
+  def new?
+    admin_or_profesor?
+  end
+
+  def create?
+    admin_or_profesor?
+  end
+
+  def edit?
+    admin_or_profesor?
+  end
+
+  def update?
+    admin_or_profesor?
+  end
+
+  def admin_or_profesor?
+    user.admin? || user.professor?
+  end
 end
